fdbc0e34ec2bdb2f5e6589ab86cbd5d6324d1674
Mantra IT Team <it-team@mantracare.com>
2026-03-26 17:56:42 +0530

    fix-v1.9-subpath

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "",
+  base: "/alcohol_consumption/",
   server: {
     host: "::",
     port: 8080,
